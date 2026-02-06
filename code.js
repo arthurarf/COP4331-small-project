42f142a1fc9249f248fda20d59befe6c009db904
NutellaLife <niky20062@gmail.com>
2026-02-05 22:07:07 -0500

    Frontend Login. First commit login needs to be tested

---
+++ code.js
@@ -0,0 +1,121 @@
+const urlBase = 'http://cop4331-huff.xyz';
+const extension = 'php';
+
+let userId = 0;
+let firstName = "";
+let lastName = "";
+
+function doLogin()
+{
+	userId = 0;
+	firstName = "";
+	lastName = "";
+	
+	let login = document.getElementById("loginName").value;
+	let password = document.getElementById("loginPassword").value;
+	
+    // --- CHANGE START: Get the result element ---
+    let resultDiv = document.getElementById("loginResult");
+    
+    // Reset the banner: clear text, hide it, and remove the red style
+	resultDiv.innerHTML = "";
+    resultDiv.style.display = "none"; 
+    resultDiv.className = ""; 
+    // --- CHANGE END ---
+
+	let tmp = {login:login,password:password};
+	let jsonPayload = JSON.stringify( tmp );
+	
+	let url = urlBase + '/Login.' + extension;
+
+	let xhr = new XMLHttpRequest();
+	xhr.open("POST", url, true);
+	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+	try
+	{
+		xhr.onreadystatechange = function() 
+		{
+			if (this.readyState == 4 && this.status == 200) 
+			{
+				let jsonObject = JSON.parse( xhr.responseText );
+				userId = jsonObject.id;
+		
+				if( userId < 1 )
+				{		
+                    // --- CHANGE START: Show Error Banner ---
+					resultDiv.innerHTML = "User/Password combination incorrect";
+                    resultDiv.className = "error-banner"; // Add the CSS class
+                    resultDiv.style.display = "block";    // Make it visible
+                    // --- CHANGE END ---
+					return;
+				}
+		
+				firstName = jsonObject.firstName;
+				lastName = jsonObject.lastName;
+
+				saveCookie();
+	
+				window.location.href = "color.html";
+			}
+		};
+		xhr.send(jsonPayload);
+	}
+	catch(err)
+	{
+        // --- CHANGE START: Show System Error Banner ---
+		resultDiv.innerHTML = err.message;
+        resultDiv.className = "error-banner";
+        resultDiv.style.display = "block";
+        // --- CHANGE END ---
+	}
+}
+
+function saveCookie()
+{
+	let minutes = 20;
+	let date = new Date();
+	date.setTime(date.getTime()+(minutes*60*1000));	
+	document.cookie = "firstName=" + firstName + ",lastName=" + lastName + ",userId=" + userId + ";expires=" + date.toGMTString();
+}
+
+function readCookie()
+{
+	userId = -1;
+	let data = document.cookie;
+	let splits = data.split(",");
+	for(var i = 0; i < splits.length; i++) 
+	{
+		let thisOne = splits[i].trim();
+		let tokens = thisOne.split("=");
+		if( tokens[0] == "firstName" )
+		{
+			firstName = tokens[1];
+		}
+		else if( tokens[0] == "lastName" )
+		{
+			lastName = tokens[1];
+		}
+		else if( tokens[0] == "userId" )
+		{
+			userId = parseInt( tokens[1].trim() );
+		}
+	}
+	
+	if( userId < 0 )
+	{
+		window.location.href = "index.html";
+	}
+	else
+	{
+//		document.getElementById("userName").innerHTML = "Logged in as " + firstName + " " + lastName;
+	}
+}
+
+function doLogout()
+{
+	userId = 0;
+	firstName = "";
+	lastName = "";
+	document.cookie = "firstName= ; expires = Thu, 01 Jan 1970 00:00:00 GMT";
+	window.location.href = "index.html";
+}
